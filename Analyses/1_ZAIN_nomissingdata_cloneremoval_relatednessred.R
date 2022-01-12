ff525cdc3fb66cb5154fb0ef1d030bcb69fd8c59
Emily Schumacher <62253972+ekschumacher@users.noreply.github.com>
2022-01-12 14:13:28 -0600

    adding new data files (updated after reruns on 1/11/22) and running missing data, clone checks and relatedness

--- Analyses/1_ZAIN_nomissingdata_cloneremoval_relatednessred.R
+++ Analyses/1_ZAIN_nomissingdata_cloneremoval_relatednessred.R
@@ -1,11 +1,10 @@
-#############This is the first script for the Zamia integrifolia in situ/ex situ project
-############We are comparing ex situ collection of wild diversity 
-###########For the rare cycad Zamia integrifolia
-##########This script will focus on individual reduction based on missing data and relatedness
+###This is the first script for the Zamia integrifolia in situ/ex situ project. 
+##We are comparing ex situ collection of wild diversity for the rare cycad Zamia integrifolia
+#This script will focus on individual reduction based on missing data and relatedness
 
-###################################
-########## Libraries ##############
-###################################
+#############################
+#         Libraries         # 
+#############################
 ##load in packages
 library(adegenet)
 library(diveRsity)
@@ -13,75 +12,103 @@ library(poppr)
 library(hierfstat)
 library(Demerelate)
 
-##################################
-####### Load in data files #######
-##################################
-##setting your working directory 
-ZAIN_wd <- "G:\\Shared drives\\Emily_Schumacher\\ZAIN"
-
+###################################
+#        Load in data files       #
+###################################
 ##set working directory
-setwd(ZAIN_wd)
+setwd("../Data_Files")
 
-##convert to a genind file 
-arp2gen("\\Data_Files\\ZAIN_garden_wild\\ZAIN_garden_wild.arp")
+##convert to a genepop file if necessary
+#arp2gen("ZAIN_adegenet_files/ZAIN_allpop.arp")
 
 ##load in genind 
-ZAIN_garden_wild_gen <- read.genepop("Data_Files\\ZAIN_garden_wild\\ZAIN_garden_wild.gen", ncode = 3)
+ZAIN_allpop_gen <- read.genepop("ZAIN_adegenet_files/ZAIN_allpop.gen", ncode = 3)
 
 ##ZAIN score df 
-ZAIN_garden_wild_df <- read.csv(paste0(ZAIN_wd, "\\Data_Files\\ZAIN_garden_wild\\ZAIN_garden_wild_df.csv"))
+ZAIN_allpop_df <- read.csv("ZAIN_data_frames/ZAIN_allpop_df.csv")
 
-##organize genind file 
-levels(ZAIN_garden_wild_gen@pop) <- unique(ZAIN_garden_wild_df$Pop)
+##create a population names list 
+ZAIN_allpop_names <- unique(ZAIN_allpop_df$Pop)
+##name populations in the genind object  
+levels(ZAIN_allpop_gen@pop) <- ZAIN_allpop_names
 
-##name the individuals in the genind 
-rownames(ZAIN_garden_wild_gen@tab) <- ZAIN_garden_wild_df$Sample.Name
+##name the individuals in the genind object
+rownames(ZAIN_allpop_gen@tab) <- ZAIN_allpop_df$Sample.Name
 
-#############################################################
-######## Remove individuals with too much missing data ######
-#############################################################
-##remove individuals with too much missing data 
-ZAIN_garden_wild_gen <- missingno(ZAIN_garden_wild_gen, type = "geno", cutoff = 0.25, quiet = FALSE, freq = FALSE)
-
-##################################
-########## Remove clones #########
-##################################
+########################################################
+#     Removes clones and ind with missing data         #
+########################################################
 ##Create a list of clones from the ZAIN inds 
-ZAIN_clones <- as.genclone(ZAIN_garden_wild_gen)
-##setting the population levels for the genind   
-strata(ZAIN_clones) <- other(ZAIN_clones)$population_hierarchy[-1]
+ZAIN_clones <- as.genclone(ZAIN_allpop_gen)
 ##create MLG list 
 ZAIN_mlg_list <- mlg.id(ZAIN_clones)
 #Function to pull out individual indices where clone length greater than 1
 ZAIN_clone_index <- which(sapply(ZAIN_mlg_list, function(x) length(x)>1))
-write.csv(ZAIN_mlg_list[ZAIN_clone_index],file = "C:\\Users\\eschumacher\\Documents\\GitHub\\ZAIN_insitu_exsitu\\Data_Files\\ZAIN_data_frames\\ZAIN_clone_list.csv")
+##create a data frame of all of the clones 
+write.csv(ZAIN_mlg_list[ZAIN_clone_index],file = "ZAIN_data_frames/ZAIN_clone_list.csv")
 
 #This removes clones and then saves as new file for Genlex if desired
-ZAIN_nocl <- clonecorrect(ZAIN_garden_wild_gen, strata=~Pop)
-genind2genalex(genclone2genind(ZAIN_nocl),file="C:\\Users\\eschumacher\\Documents\\GitHub\\ZAIN_insitu_exsitu\\Data_Files\\ZAIN_geninds\\ZAIN_garden_wild_nocl.csv")
+ZAIN_nocl_gen <- clonecorrect(ZAIN_allpop_gen)
 
-###################################################
-############ Relatedness Reduction ################
-###################################################
-###reduce relatedness
-ZAIN_reorg_relate_df <- Demerelate(ZAIN_garden_wild_df, object = T, value = "loiselle")
+##remove individuals with too much missing data 
+ZAIN_allpop_clean_gen <- missingno(ZAIN_nocl_gen, type = "geno", cutoff = 0.25, quiet = FALSE, freq = FALSE)
+##write out genepop file that's been cleaned for missing data and clones 
+genind2genalex(ZAIN_allpop_clean_gen, "ZAIN_data_frames/ZAIN_allpop_clean_genalex.csv")
 
-##now identify how many individuals have greater than 25% relatedness = half siblings
-ZAIN_halfsib_names <- names(which(unlist(ZAIN_reorg_relate_df$Empirical_Relatedness) > 0.25))
+##write out data frame and genepop files that are cleaned for missing data and clones 
+ZAIN_allpop_clean_df <- ZAIN_allpop_df[ZAIN_allpop_df$Sample.Name %in% rownames(ZAIN_allpop_clean_gen@tab),]
+##write out data frame cleaned for missing data and clones 
+write.csv(ZAIN_allpop_clean_df, "ZAIN_data_frames/ZAIN_allpop_clean_df.csv")
 
-##then use this to create a document which has all the unique individual numbers for every highly related individuals
-ZAIN_halfsib_names_cleanfront <- gsub("^.*\\.","", ZAIN_halfsib_names)
+######################################
+#       Relatedness Reduction        #
+######################################
+##limit data frames 
+ZAIN_garden_clean_df <- ZAIN_allpop_clean_df[ZAIN_allpop_clean_df$Garden_Wild == "Garden",]
+ZAIN_wild_clean_df <- ZAIN_allpop_clean_df[ZAIN_allpop_clean_df$Garden_Wild == "Wild",]
 
-ZAIN_halfsib_names_cleanback <- gsub("^.*\\_","", ZAIN_halfsib_names_cleanfront)
+##limit by individuals and populations 
+ZAIN_garden_clean_gen <- ZAIN_allpop_clean_gen[rownames(ZAIN_allpop_clean_gen@tab) %in% ZAIN_garden_clean_df$Sample.Name,]
+ZAIN_wild_clean_gen <- ZAIN_allpop_clean_gen[rownames(ZAIN_allpop_clean_gen@tab) %in% ZAIN_wild_clean_df$Sample.Name,]
 
-ZAIN_relate_ind_remove <- unique(ZAIN_halfsib_names_cleanback)
+##create lists of these objects 
+ZAIN_df_list <- list(ZAIN_garden_clean_df, ZAIN_wild_clean_df)
+ZAIN_gen_list <- list(ZAIN_garden_clean_gen, ZAIN_wild_clean_gen)
 
-##then subset genind file
-butternutgen_relatedness_reduced <- butternutgen_nomd[!rownames(butternutgen_nomd@tab) %in% relate_ind_remove,]
+##list of pop type 
+pop_type_list <- c("Garden", "Wild")
 
-##subset data frame 
-reorg_relatedness_reduced <- reorg_relatedness[!reorg_relatedness$Ind %in% relate_ind_remove,]
-
-###name pops
-levels(butternutgen_relatedness_reduced@pop) <- butternut_24pop_names
-
+for(pop_type in 1:length(pop_type_list)){
+  
+  ##create data frames separated by type - garden or wild 
+  ZAIN_df <- ZAIN_allpop_clean_df[ZAIN_allpop_clean_df$Garden_Wild == paste0(pop_type_list[[pop_type]]),]
+  
+  ##limit genind objects by garden of wild type 
+  ZAIN_gen <- ZAIN_allpop_clean_gen[rownames(ZAIN_allpop_clean_gen@tab) %in% ZAIN_df$Sample.Name,]
+  
+  ##now run relatedness analysis on the data frame 
+  ZAIN_relate_df <- Demerelate(ZAIN_df[,-3], object = T, value = "loiselle")
+  
+  ##now identify how many individuals have greater than 25% relatedness = half siblings
+  ZAIN_halfsib_names <- names(which(unlist(ZAIN_relate_df$Empirical_Relatedness) > 0.25))
+  
+  ##create a list of related individual names 
+  ZAIN_halfsib_names_cleanfront <- gsub("^.*\\.","", ZAIN_halfsib_names)
+  
+  ZAIN_halfsib_names_cleanback <- gsub("^.*\\_","", ZAIN_halfsib_names_cleanfront)
+  
+  ZAIN_relate_ind_remove <- unique(ZAIN_halfsib_names_cleanback)
+  
+  ##first subset the genind object with the list 
+  ZAIN_rel_gen <- ZAIN_gen[!rownames(ZAIN_gen@tab) %in% ZAIN_relate_ind_remove,]
+  
+  #write a genalex file 
+  genind2genalex(ZAIN_rel_gen, paste0("ZAIN_data_frames/ZAIN_rel_", pop_type_list[[pop_type]], "_genalex.csv"))
+  
+  ##also reduce data frames by relatedness
+  ZAIN_rel_df <- ZAIN_df[ZAIN_df$Sample.Name %in%  ZAIN_relate_ind_remove,]
+  
+  ##write out data frame reduced for highly related individuals 
+  write.csv(ZAIN_rel_df, paste0("ZAIN_data_frames/ZAIN_rel_", pop_type_list[[pop_type]], "_df.csv"))
+  
+}
